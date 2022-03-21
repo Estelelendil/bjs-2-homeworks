a18b80a65315fb8b194ffe0ce7ef5433ced70d10
Чернова Александра <estelelendil@mail.ru>
2022-03-21 15:11:34 +0300

    First task finished and tested

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+'use strict'
+function solveEquation(a, b, c){
+    const arr = [];
+    const d = b ** 2 - 4 * a * c;
+
+    if ( d == 0) {
+        let answer = -b / (2 * a);
+        arr.push(answer);
+    }
+    if (d > 0){
+        let answer = (-b + Math.sqrt(d) )/(2*a)
+        arr.push(answer);
+        answer = (-b - Math.sqrt(d) )/(2*a)
+        arr.push(answer);
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
