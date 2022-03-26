5b8031875d0f642b962c769c58a66147ce9c1f57
Чернова Александра <estelelendil@mail.ru>
2022-03-26 18:45:37 +0300

    Second task is finished

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,29 @@
-function compareArrays(arr1, arr2) {
-  let result;
+// function compareArrays(arr1, arr2) {
+//   function isElement(currentElementArr,){
+// arr1.every()
+//   }
+//   // arr1.every(element) === arr2.every(element)
+//   let result;
 
   // Ваш код
 
-  return result; // boolean
-}
+//   return result; // boolean
+// }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+  
+  return resultArr; 
 }
+
+// function every(arr, func) {
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!func(arr[i], i, arr)) {
+//       return false;
+//     }
+    
+//   }
+
+//   return true;
+// }
