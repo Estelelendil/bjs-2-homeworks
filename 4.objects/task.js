113d1b0f0d8b9f29eeff61b607f9e4cfa5b1f542
Estelelendil <96063364+Estelelendil@users.noreply.github.com>
2022-04-01 13:27:12 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,13 +4,13 @@ function Student(name, gender, age) {
     this.gender = gender;
     this.age = age;
 }
-// let student1 = new Student("Tony", "male", 37)
+
 
 // --2--
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
-// student1.setSubject("Algebra");
+
 
 // --3--4--
 Student.prototype.addMark = function(mark){
@@ -33,11 +33,6 @@ Student.prototype.addMarks = function(... mark){
     }
 }
 
-// student1.addMark(5,4,5);
-// student1.addMark(4);
-// student1.addMark(5);
-//Создайте метод addMarks(mark1,mark2,mark3...) по аналогии с п.2, который при вызове будет добавлять студенту сразу несколько оценок. 
-// Подсказка: так как количество добавляемых оценок неизвестно, воспользуйтесь rest(?) параметром.
 
 // --5--
 Student.prototype.getAverage = function(){
@@ -47,11 +42,7 @@ Student.prototype.getAverage = function(){
   });
 
   return sum / this.marks.length;
-//   const initialValue = 0;
-//   return (this.marks.reduce(
-//     (previousValue, currentValue) => previousValue + currentValue,
-//     initialValue
-//   ) / this.marks.length);
+
 }
 
 // --6--
@@ -61,7 +52,7 @@ Student.prototype.exclude = function(reason){
   delete this.marks
 }
 
-// console.log(student1.getAverage());
+
 let student1 = new Student("Tony", "male", 37);
 student1.setSubject("Algebra");
 student1.addMark(5);
@@ -69,7 +60,7 @@ student1.addMark(4);
 student1.addMark(5);
 console.log(student1.getAverage()); //4.6666
 console.log(student1);
-// {age: 37, gender: "male", marks: [5, 4, 5], name: "Tony", subject: "Algebra"}
+
 let student2 = new Student("Buzz", "female", 35);
 student2.setSubject("Geometry");
 student2.addMark(2);
@@ -77,4 +68,3 @@ student2.addMark(3);
 student2.addMark(2);
 student2.exclude('low grades')
 console.log(student2)
-// {name: "Buzz", gender: "female", age: 35, excluded: "low grades"}
